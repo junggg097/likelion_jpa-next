e7a99a3060d08b0d4f587952cbd8219f6d51467a
정두콩 <junggg097@daum.net>
2024-02-01 10:52:03 +0900

    JoinTable + Columns

---
+++ src/main/java/com/example/jpanext/school/entity/AttendingLectures.java
@@ -0,0 +1,30 @@
+package com.example.jpanext.school.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "attending_lectures")
+public class AttendingLectures {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "lecture_id")
+    private Lecture lecture;
+
+    private Integer midTermScore;
+    private Integer finalsScore;
+}
